3ee3474bbddc44db69425a75c6d281664c3c0b59
Mahdi Nemati <nematimahdi88@gamil.com>
2021-02-17 21:05:50 +0330

    delete and move to commands

--- resources/VGadmin/chat/chat.mjs
+++
@@ -1,9 +0,0 @@
-import * as alt from 'alt';
-import chat from 'chat';
-import vg from 'VGfunction';
-
-function adminchat(player, ...msg) {
-    vg.adminchat(player, ...msg);
-}
-
-chat.registerCmd('a', adminchat);
