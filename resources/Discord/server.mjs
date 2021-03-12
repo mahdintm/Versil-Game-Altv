c414b0f0b28eba7125a231aae996f06b08a289d1
Mahdi Nemati <nematimahdi88@gamil.com>
2021-03-13 01:06:14 +0330

    add more code for discord resource

--- resources/Discord/server.mjs
+++ resources/Discord/server.mjs
@@ -2,6 +2,7 @@ import * as alt from 'alt';
 import Discord from 'discord.js';
 import config from './config.json';
 import dcom from './Commands/discord/commands.mjs'
+// import scom from './Commands/server/commands.mjs'
 import func from './functions/funcs.mjs';
 
 
@@ -23,11 +24,24 @@ client.on('message', message => {
             .split(" ");
         if (CMD_NAME === "discord") {
             if (args[0] === "test") {
-                const member = message.guild.members.cache.get(func.getUserFromMention(args[1]));
                 var hook = new Discord.WebhookClient("818015361307246642", "0d_bmgVzAuJuvFC-9WtrWGlhuNiax6xv9M_0EwyRRz6aXotHwoXDik7g_4dxUkERsJLT")
-                hook.send(member.avatarURL)
 
+                const embed = new Discord.MessageEmbed()
+                    .setColor('#0099ff')
+                    .setTitle('Admin System')
+                    .setURL('https://discord.js.org/')
+                    .setAuthor('Mahdi Nemati', 'http://versil.ir/avatar.jpg', 'https://discord.js.org')
+                    .setDescription('@Mahdi بن شد.')
+                    .setThumbnail()
+                    .addFields({ name: 'دلیل', value: 'چیت.' }, { name: '\u200B', value: '\u200B' }, { name: 'تاریخ بن شدن', value: 'xxxx/xx/xx', inline: true }, { name: 'تاریخ ازاد شدن', value: 'xxxx/xx/xx', inline: true })
+                    .setTimestamp()
+                    .setFooter('Versil Game Log', 'https://forum.versil.ir/uploads/monthly_2020_08/facicon.png');
 
+                hook.send('Webhook test', {
+                    username: 'Versil Game',
+                    avatarURL: 'https://forum.versil.ir/uploads/monthly_2020_08/facicon.png',
+                    embeds: [embed],
+                });
             } else if (args[0] === "kick") {
                 //kick system
 
@@ -50,6 +64,9 @@ client.on('message', message => {
                 dcom.changename(message, args)
             }
         } else if (CMD_NAME == "server") {
+            // if (args[0] === "sethp") {
+            //     scom.sethp(message, args);
+            // }
 
         }
     }
@@ -57,9 +74,7 @@ client.on('message', message => {
 });
 
 
-// export function addhook() {
 
-// }
 
 client.login(config.token);
 
